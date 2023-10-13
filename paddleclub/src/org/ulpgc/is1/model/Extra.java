ec0f7acb91607caa3a5af5366f78971b2e26c1bc
TheTatsujin <luis.martin119@alu.ulpgc.es>
2023-10-13 14:19:48 +0100

    Added Reservation list to Extra

--- paddleclub/src/org/ulpgc/is1/model/Extra.java
+++ paddleclub/src/org/ulpgc/is1/model/Extra.java
@@ -1,11 +1,16 @@
 package org.ulpgc.is1.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Extra{
     private int price;
+    private List<Reservation> reservations;
     abstract public String getName();
 
     public Extra(int price) {
         this.price = price;
+        this.reservations = new ArrayList<>();
     }
 
     public int getPrice() {
@@ -15,4 +20,17 @@ public abstract class Extra{
     public void setPrice(int price) {
         this.price = price;
     }
+
+    public Reservation getReservation(int index){
+        return this.reservations.get(index);
+    }
+
+    public int reservationCount(){
+        return this.reservations.size();
+    }
+
+    public void addReservation(Reservation newReservation){
+        this.reservations.add(newReservation);
+    }
+
 }
