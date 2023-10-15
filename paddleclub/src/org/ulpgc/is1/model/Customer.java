f1d491f493fb23d51b3ce753a43bdb8827e7a5cc
mario <mario2004mario16@hotmail.com>
2023-10-15 12:33:10 +0100

    Clase Customer acabada

--- paddleclub/src/org/ulpgc/is1/model/Customer.java
+++ paddleclub/src/org/ulpgc/is1/model/Customer.java
@@ -1,4 +1,53 @@
 package org.ulpgc.is1.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Customer {
+    private String name;
+    private String surname;
+    private NIF nif;
+    private List<Reservation> reservations;
+
+    public Customer(String name, String surname, NIF nif) {
+        this.name = name;
+        this.surname = surname;
+        this.nif = nif;
+        this.reservations = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public NIF getNif() {
+        return nif;
+    }
+
+    public void setNif(NIF nif) {
+        this.nif = nif;
+    }
+    public Reservation getReservation(int index){
+        return this.reservations.get(index);
+    }
+
+    public int reservationCount(){
+        return this.reservations.size();
+    }
+
+    public void addReservation(Reservation newReservation){
+        this.reservations.add(newReservation);
+    }
 }
