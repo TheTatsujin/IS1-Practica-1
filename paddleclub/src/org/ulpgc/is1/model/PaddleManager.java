07748892e1c32ac3945a45b43ace92f44fa21cd1
mario <mario2004mario16@hotmail.com>
2023-10-16 11:31:26 +0100

    Clase Reservation terminada

--- paddleclub/src/org/ulpgc/is1/model/PaddleManager.java
+++ paddleclub/src/org/ulpgc/is1/model/PaddleManager.java
@@ -1,4 +1,30 @@
 package org.ulpgc.is1.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PaddleManager {
+    private List<Customer> customerList;
+    private List<Court> courtList;
+
+    public PaddleManager(){
+        customerList = new ArrayList<>();
+        courtList = new ArrayList<>();
+    }
+
+    public void addCustomer(String name, String surname, NIF nif){
+        customerList.add(new Customer(name, surname, nif));
+    }
+    public void addCourt(String name, int price, CourtType type){
+        courtList.add(new Court(name, price, type));
+    }
+    public Customer getCustomer(){
+
+    }
+    public Court getCourt(){
+
+    }
+    public Reservation reserve(){
+
+    }
 }
--- paddleclub/src/org/ulpgc/is1/model/Reservation.java
+++ paddleclub/src/org/ulpgc/is1/model/Reservation.java
@@ -1,4 +1,33 @@
 package org.ulpgc.is1.model;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Reservation {
+    private static int NEXT_ID = 0;
+    private Date date;
+    private final int id;
+    private List<Extra> extras;
+    public Reservation(Date date, int id) {
+        NEXT_ID++;
+        this.date = date;
+        this.id = id;
+        this.extras = new ArrayList<>();
+    }
+    public void addExtra(Extra extra){
+        this.extras.add(extra);
+    }
+    public void removeExtra(int indx){
+        this.extras.remove(indx);
+    }
+    public void removeExtra(Extra extra){
+        this.extras.remove(extra);
+    }
+    public int price(){
+        int resultado = 0;
+        for (Extra i: this.extras) {
+            resultado += i.getPrice();
+        } return resultado;
+    }
 }
